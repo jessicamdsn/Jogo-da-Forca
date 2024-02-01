bfb1a4f5ce42589cea81c0173107844780ae23f8
Jessica neves <128631606+jessicamdsn@users.noreply.github.com>
2024-02-01 00:23:44 -0300

    game working with menu

--- js/jogo.js
+++ js/jogo.js
@@ -1,17 +1,72 @@
-var letras = document.querySelectorAll('.letras');
+var letras = document.querySelector('.letras');
 var letra = document.querySelectorAll('.letra');
+var aviso = document.querySelector('.aviso')
 
+var menuItem = document.querySelectorAll('.item-menu');
+var tema = document.getElementById('tema');
+var thisElement;
+var temaSelecionado;
+var palavraSecreta = "";
 
 const frutas = ["BANANA", "UVA", "PITANGA", "MELANCIA", "LARANJA"];
-const palavraSecreta =
-  frutas[Math.floor(Math.random() * frutas.length)];
+const aleatorio = ["BOLA", "RESTAURANTE", "PETECA", "PAMONHA", "CINEMA"];
+const paises = ["BRASIL", "URUGUAI", "MEXICO", "PORTUGAL", "CHINA"];
+const adjetivos = ["BONITO", "ALTO", "CRIATIVO", "CARINHOSO", "TIMIDO"];
+const animais = ["GATO", "CACHORRO", "COELHO", "CAVALO", "FUINHA"];
+const profissoes = ["MEDICO", "ENGENHEIRO", "MOTORISTA", "PROGRAMADOR", "VENDEDOR"];
 const letrasErradas = [];
 const letrasCorretas = [];
 
+
+function selectLink() {
+    menuItem.forEach((item) =>
+        item.classList.remove('ativo'))
+    this.classList.add('ativo'); 
+    aviso.textContent ='Digite uma letra para iniciar';
+    tema.textContent = 'Tema: ' + this.innerText;
+    thisElement=this; 
+    
+    if (letrasCorretas.length > 0 || letrasErradas.length > 0) {
+        // Se já foram digitadas letras, reinicia o jogo
+        reiniciarJogoExecutado();
+    } else {
+        // Se não, apenas atualiza os itens do jogo
+        aparecerItensJogo();
+    }
+    
+
+    switch (thisElement.innerText) {
+        case 'Aleatorio':
+            temaSelecionado = aleatorio;
+            break;
+        case 'Frutas':
+            temaSelecionado = frutas;
+            break;
+        case 'Paises':
+            temaSelecionado = paises;
+            break;
+        case 'Adjetivos':
+            temaSelecionado = adjetivos;
+            break;
+        case 'Animais':
+            temaSelecionado = animais;
+            break;
+        case 'Profissoes':
+            temaSelecionado = profissoes;
+            break;
+        default:
+           alert('não pegou')
+    }
+    palavraSecreta = temaSelecionado[Math.floor(Math.random() * temaSelecionado.length)];
+    
+}
+menuItem.forEach((item) =>
+    item.addEventListener('click', selectLink))
+    
 letra.forEach((letra) => {
     letra.addEventListener('click', (event) => {
         const letraClicada = event.target.textContent;
-        
+
         if (letrasErradas.includes(letraClicada) || letrasCorretas.includes(letraClicada)) {
             mostrarAvisoLetraRepetida();
         } else {
@@ -23,61 +78,93 @@ letra.forEach((letra) => {
                 letra.classList.add('errado');
             }
         }
-        
+
         atualizarJogo();
     });
 });
 
-function atualizarJogo(){
+function atualizarJogo() {
     mostrarLetrasCertas();
     desenharForca();
     checarJogo();
+    apagarAviso()
+    aparecerItensJogo()
 }
-function mostrarLetrasCertas(){
-const containerPalavraSecreta = document.querySelector(".palavra-secreta-container");
-containerPalavraSecreta.innerHTML = "";
-palavraSecreta.split("").forEach((letraClicada) => {
-  if (letrasCorretas.includes(letraClicada)) {
-    containerPalavraSecreta.innerHTML += ` ${letraClicada} `;
-  } else {
-    containerPalavraSecreta.innerHTML += ` _ `;
-  }
-});
+function reiniciarJogoExecutado() {
+
+    aviso.textContent = 'Digite uma letra para iniciar';
+    tema.textContent = 'Tema: ' + thisElement.innerText;
+    aparecerItensJogo();
+    
+    // Reiniciar arrays
+    letrasErradas.length = 0;
+    letrasCorretas.length = 0;
+
+    aviso.style.display = 'block';
+    document.querySelector(".palavra-secreta-container").innerHTML = "";
+    
+    const partesCorpo = document.querySelectorAll(".forca-parte");
+    partesCorpo.forEach((parte) => {
+        parte.style.display = "none";
+    });
+    letra.forEach((letra) => {
+        letra.classList.remove('certo', 'errado');
+    });
+    
+}
+function aparecerItensJogo(){
+    const linhas = document.querySelectorAll(".linha");
+    linhas.forEach(linha => linha.style.display = 'block');
+    letras.style.display = 'block';
+}
+function apagarAviso(){
+    //aviso.textContent ='';
+    aviso.style.display = 'none';
+}
+function mostrarLetrasCertas() {
+    const containerPalavraSecreta = document.querySelector(".palavra-secreta-container");
+    containerPalavraSecreta.innerHTML = "";
+    palavraSecreta.split("").forEach((letraClicada) => {
+        if (letrasCorretas.includes(letraClicada)) {
+            containerPalavraSecreta.innerHTML += ` ${letraClicada} `;
+        } else {
+            containerPalavraSecreta.innerHTML += ` _ `;
+        }
+    });
 }
 function checarJogo() {
     let mensagem = "";
     const containerPalavraSecreta = document.querySelector(".palavra-secreta-container");
     const partesCorpo = document.querySelectorAll(".forca-parte");
-  
+
     if (letrasErradas.length === partesCorpo.length) {
-      mensagem = "Fim de jogo! Você perdeu!";
+        mensagem = "Fim de jogo! Você perdeu!";
     }
     if (palavraSecreta.split("").every(letra => letrasCorretas.includes(letra))) {
-    //if (palavraSecreta === containerPalavraSecreta.innerText) {
-      mensagem = "Parabéns! Você ganhou!";
+        //if (palavraSecreta === containerPalavraSecreta.innerText) {
+        mensagem = "Parabéns! Você ganhou!";
     }
-  
+
     if (mensagem) {
-      document.querySelector("#mensagem").innerHTML = mensagem;
-      document.querySelector(".popup-container").style.display = "flex";
+        document.querySelector("#mensagem").innerHTML = mensagem;
+        document.querySelector(".popup-container").style.display = "flex";
     }
-  }
-  
+}
 
 function desenharForca() {
     const partesCorpo = document.querySelectorAll(".forca-parte");
     for (let i = 0; i < letrasErradas.length; i++) {
-      partesCorpo[i].style.display = "block";
+        partesCorpo[i].style.display = "block";
     }
-  }
+}
 
-function mostrarAvisoLetraRepetida(){
+function mostrarAvisoLetraRepetida() {
     alert("Você já usou essa letra");
 }
 function reiniciarJogo() {
     window.location.reload();
-  }
-  
+}
+
 
 
 
--- js/menu.js
+++ js/menu.js
@@ -1,24 +1,11 @@
-var menuItem = document.querySelectorAll('.item-menu');
-var tema = document.getElementById('tema');
 
-function selectLink(){
 
-    menuItem.forEach((item)=>
-    item.classList.remove('ativo') //remove classlist do q n clicou
-    )
-    this.classList.add('ativo')  // add classlist do clicado
-    tema.textContent = 'Tema: ' + this.innerText;
-}
-
-menuItem.forEach((item)=>
-item.addEventListener('click',selectLink)
-)
 
 //expandir menu
 var btnExp = document.querySelector('#btn-exp');
 var menuLateral = document.querySelector('.menu-lateral');
 
-btnExp.addEventListener('click',function(){
+btnExp.addEventListener('click', function () {
     //ao apertar p botão se existir a classe expandir- remove, se n existir -adiciona
     menuLateral.classList.toggle('expandir')
     //menuLateral.forEach(x => x.classList.toggle('expandir'))
